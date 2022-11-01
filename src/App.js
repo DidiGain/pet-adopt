e7c58f0e8048a01422aeee78be56550841c0e135
Dinara Gainulina <dignara@yandex.ru>
2022-11-01 15:31:49 +0300

    feat: add react query

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
 import { StrictMode } from 'react';
-import { render } from 'react-dom';
+import { render, createRoot } from 'react-dom';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import ThemeContext from './ThemeContext';
+import AdoptedPetContext from './components/AdoptedPetContext';
+import WrappedDetails from './components/Details';
+import SearchParams from './components/SearchParams';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
+  const adoptedPet = useState(null);
+
   return (
-    <StrictMode>
+    <div>
       <BrowserRouter>
-        <header>
-          <Link to="/">Adopt me!</Link>
-        </header>
-        <Routes>
-          <Route path="" element={} />
-          <Route path="" element={} />
-        </Routes>
+        <AdoptedPetContext.Provider value={adoptedPet}>
+          <QueryClientProvider client={queryClient}>
+            <header>
+              <Link to="/">Adopt me!</Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<WrappedDetails />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </QueryClientProvider>
+        </AdoptedPetContext.Provider>
       </BrowserRouter>
-    </StrictMode>
+    </div>
   );
 };
 
-render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
--- src/ThemeContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-const ThemeContext = createContext(['sea', () => {}]);
-
-export default ThemeContext;
