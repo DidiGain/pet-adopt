9fc1bb4f72d97ed5c3dd997bea0c1bd5a98b8299
Dinara Gainulina <dignara@yandex.ru>
2022-11-02 17:53:23 +0300

    feat: add styles

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -6,13 +6,20 @@ const Pet = ({ id, name, animal, breed, location, images }) => {
   if (images.length) hero = images[0];
 
   return (
-    <Link to={`/details/${id}`}>
-      <div>
-        <img src={hero} alt={name} />
+    <Link
+      to={`/details/${id}`}
+      className="grid grid-rows-[83%, 17%] pb-10 bg-slate-100 rounded overflow-hidden"
+    >
+      <div className="w-full h-7/8 mb-5 bg-gray-300">
+        <img
+          src={hero}
+          alt={name}
+          className="w-full h-full bg-gray-300 object-cover"
+        />
       </div>
-      <div className="info">
-        <h1>{name}</h1>
-        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      <div className="mt-auto flex flex-col items-center justify-around gap-2 flex-wrap px-3">
+        <h1 className="text-2xl text-pink-600 font-bold">{name}</h1>
+        <h2 className="md:text-xs sm:text-xs lg:text-base text-[#333] text-center">{`${animal}: ${breed} from ${location}`}</h2>
       </div>
     </Link>
   );
